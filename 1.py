190d59e96012b2bf00b8230d85737bf9de432bad
Janit8r <1206577452@qq.com>
2023-07-09 13:30:49 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 
                 #print(type(login_res.text))
                 token = json.loads(login_res.text)['data']['token']
-				print("token："+str(token))
+		print("token："+str(token))
                 print("[+] Login Success!!")
 
                 # main
