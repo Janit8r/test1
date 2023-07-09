1041fd8daa2eb559b28b920a8010f87ff510a74a
Janit8r <1206577452@qq.com>
2023-07-09 12:35:30 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -8,7 +8,7 @@ data='{"username":"admin","password":"arlpass"}'
 headers=(
     "Content-Type: application/json; charset=UTF-8"
     "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36"
-    "Origin: https://10.147.19.32:5003"
+    "Origin: https://127.0.0.1:5003"
 )
 
 # 发送请求并提取响应包Token
