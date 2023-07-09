67032d221ed050559ff65c955d186564ca225c55
Janit8r <1206577452@qq.com>
2023-07-09 12:49:15 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,18 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-# 设置请求URL和请求体
-url="https://127.0.0.1:5003/api/user/login"
-data='{"username":"admin","password":"arlpass"}'
-
-# 设置请求头部信息
-headers=(
-    "Content-Type: application/json; charset=UTF-8"
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36"
-    "Origin: https://127.0.0.1:5003"
-)
-
-# 发送请求并提取响应包Token
-token=$(curl -s -X POST -H "${headers[@]}" -d "${data}" "${url}" | jq -r '.token')
-
-# 输出响应包Token
-echo "Token: ${token}"
+curl -X POST -H "Content-Length: 41" -H "Sec-Ch-Ua: \"Chromium\";v=\"109\", \"Not_A Brand\";v=\"99\"" -H "Accept: application/json, text/plain, */*" -H "Content-Type: application/json; charset=UTF-8" -H "Sec-Ch-Ua-Mobile: ?0" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.75 Safari/537.36" -H "Token: 213b7a8753c8e0f450de82b50ed35abd" -H "Sec-Ch-Ua-Platform: \"Windows\"" -H "Origin: https://10.147.19.32:5003" -H "Sec-Fetch-Site: same-origin" -H "Sec-Fetch-Mode: cors" -H "Sec-Fetch-Dest: empty" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: zh-CN,zh;q=0.9" -d '{"username":"admin","password":"arlpass"}' https://127.0.0.1:5003/api/user/login
